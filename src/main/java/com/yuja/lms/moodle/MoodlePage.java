1527a54d548032721554435ec3829c3520113055
SaleenaGeorge <saleena.george@yuja.com>
2023-03-07 23:09:37 -0500

    include test case for course and user provisioning in moodle
    1.auto provisioning of course
    2.manual provisioning to existing course
    3.manual provisioning to new course
    4.User provisioning as Manager, Teacher and student

--- src/main/java/com/yuja/lms/moodle/MoodlePage.java
+++ src/main/java/com/yuja/lms/moodle/MoodlePage.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 import org.openqa.selenium.Alert;
@@ -11,9 +12,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 
 import com.yuja.evp.modalhelpers.AddMediaModalHelperMethods;
 import com.yuja.evp.pagehelpers.MediaLibraryPageHelpers;
+import com.yuja.evp.pagehelpers.NavigationBarHelpers;
 import com.yuja.evp.pagehelpers.QuizPageHelpers;
 import com.yuja.evp.pagetestmethods.MediaLibraryPageTestMethods;
 import com.yuja.evp.utilities.Helpers;
@@ -22,7 +26,9 @@ public class MoodlePage extends QuizPageHelpers {
 	
 	private WebElement mediaUploadModal = null;
 	
+	
 	MediaLibraryPageHelpers mediaLibrary = new MediaLibraryPageHelpers();
+	NavigationBarHelpers navbar=new NavigationBarHelpers();
 	
 	private void setMediaUploadModal() {
 		System.out.println("Fetching Media Upload modal...");
@@ -55,6 +61,53 @@ public class MoodlePage extends QuizPageHelpers {
 		String URL = "https://tmoodle2.yuja.com/course/view.php?id=142";
 		launchUrl(URL, "Moodle automation course page");
 	}
+	public void selectCourseFromSiteHome(String courseName) {
+		clickElement("site home", By.xpath("//a[@href=\"https://tmoodle2.yuja.com/?redirect=0\"]"));
+		List<WebElement> courseList= getElementList(By.xpath("//a[@class='aalink']"));
+		for(int i=0;i<courseList.size();i++) {
+			WebElement element=courseList.get(i);
+			String obtainedCourseName = element.getText();
+			if(obtainedCourseName.equals(courseName)) {
+				clickElement("click on given course",element);
+				break;
+			}
+			}
+	}
+	
+	public void createMoodleCourse(String adminUserName, String adminPassword, String courseName, String courseShortName) throws InterruptedException{
+		navigateToLoginPage();
+		driver.manage().window().maximize();
+		loginFast(adminUserName, adminPassword);
+		waitForElement(By.xpath("//span[@class='site-name d-none d-md-inline']"), 10);
+		String URL = "https://tmoodle2.yuja.com/admin/search.php#linkcourses";
+		launchUrl(URL, "Moodle automation course creation page");
+		clickElement("Add a new course button", By.xpath("//a[text()='Add a new course']"));
+		sendKeys("Enter course full name", By.xpath("//input[@name='fullname']"),courseName+getRandomInteger(1000));
+		sendKeys("Enter course short name", By.xpath("//input[@name='shortname']"),courseShortName+getRandomInteger(1000));
+		clickElement("save and display button", By.xpath("//input[@name='saveanddisplay']"));
+		clickElement("Course media page", By.xpath("//a[@data-key='coursehome']"));
+		
+	}
+	public String createMoodleUser(String adminUserName, String adminPassword,  String email, String newUsername, String newUserPassword) throws InterruptedException{
+		navigateToLoginPage();
+		driver.manage().window().maximize();
+		loginFast(adminUserName, adminPassword);
+		waitForElement(By.xpath("//span[@class='site-name d-none d-md-inline']"), 10);
+		String URL = "https://tmoodle2.yuja.com/admin/search.php#linkusers";
+		launchUrl(URL, "Moodle automation user creation page");
+		clickElement("add a new user button", By.xpath("//a[text()='Add a new user']"));
+		String newUserNameRandom=newUsername+getRandomInteger(1000);
+		sendKeys("Enter user name", By.xpath("//input[@name='username']"),newUserNameRandom);
+		WebElement passwordField=driver.findElement(By.xpath("//em[text()='Click to enter text']"));
+		clickElement("Enter password",passwordField);
+		typeKeys(newUserPassword);
+		sendKeys("Enter first name", By.xpath("//input[@name='firstname']"),newUserNameRandom);
+		sendKeys("Enter surname", By.xpath("//input[@name='lastname']"),newUserNameRandom);
+		sendKeys("Enter email", By.xpath("//input[@name='email']"),email+getRandomInteger(10000));
+		clickElement("save and display button", By.xpath("//input[@name='submitbutton']"));
+		return newUserNameRandom;
+		
+	}
 	
 	public void NavigateToLTI(String embedMediaTitle){
 		clickEmbedMediaMoodle(embedMediaTitle);
@@ -66,6 +119,7 @@ public class MoodlePage extends QuizPageHelpers {
 		clickElement("grades button", By.xpath("(//span[text()='Grades'])[2]"));
 		switchToIframe("switch to lti frame", By.id("contentframe"), 10);
 	}
+	
 	
 	
 	//used directly in test class. Login create publish embed attend and access the yuja gradebook and check lms grade sync via activity log
@@ -177,6 +231,159 @@ public class MoodlePage extends QuizPageHelpers {
 		Thread.sleep(4000);
 	}
 	
+	public void setAndResetAutomaticProvision(String userName,String password,String userOrcourse,Boolean enabledOrDisabled) {
+		HashMap<String, String> provisionType = new HashMap<String, String>();
+		provisionType.put("user", "(//input[@id='autoProvisionId'])[2]");
+		provisionType.put("course", "(//input[@id=\"autoProvisionClassesId\"])[2]");
+	    mediaLibrary.navigateToMyMediaUserLogin(userName, password);
+	    URL = "https://staging-demo.yuja.com/P/Institution/APIManagementServlet/";
+        launchUrl(URL, "Xavier University Enterprise Video Platform");
+        Select integrationDropdown = new Select(driver.findElement(By.id("apiPicker")));
+        integrationDropdown.selectByValue("lti3");
+        WebElement checkBox=driver.findElement(By.xpath(provisionType.get(userOrcourse)));
+        boolean autoProvisionCourseCheckboxSelected=checkBox.isSelected();
+        System.out.println(autoProvisionCourseCheckboxSelected);
+        if(autoProvisionCourseCheckboxSelected^enabledOrDisabled) {
+    	clickElement("checkbox",checkBox );
+        clickElement("save",By.xpath("//form[@id=\"lti3SettingsGeneral\"]//button[@title=\"Save\"]") );
+        clickElement("save confirm",By.xpath("//div[@class=\"modal-content\"]//button[@title=\"Confirm\"]") );}
+        else {
+			reportStep("The checkbox is setting was same as required","PASS",false);
+		}	
+	}
+	
+
+	
+	public void setStartPageOptionAsMediaChannel() {
+        URL = "https://staging-demo.yuja.com/P/Institution/MenuManagement/";
+        launchUrl(URL, "Xavier University Enterprise Video Platform");
+        Select startPageOptionITManager = new Select(driver.findElement(By.id("select_defaultLandingPageIT")));
+        startPageOptionITManager.selectByValue("mediaChannel");
+        Select startPageOptionInstructor = new Select(driver.findElement(By.id("select_defaultLandingPageInstructor")));
+        startPageOptionITManager.selectByValue("mediaChannel");
+        Select startPageOptionStudent = new Select(driver.findElement(By.id("select_defaultLandingPageStudent")));
+        startPageOptionITManager.selectByValue("mediaChannel");
+        clickElement("save",By.xpath("//form[@id=\"saveSetupPrefences\"]//button[@id=\"setupPrefences_saveButton\"]"));
+     }
+	
+	// Method directly used in test class.
+	public void checkAutoprovisionOfCourse(String adminUserName, String adminPassword, String courseName, String courseShortName, String externalToolCustomName, String externalToolVisibleName, String userName, String password ) throws InterruptedException {
+		setAndResetAutomaticProvision(userName,password,"course",true);
+		setStartPageOptionAsMediaChannel();
+		createMoodleCourse(adminUserName, adminPassword,courseName,courseShortName);
+		String moodleCourseName=driver.findElement(By.xpath("//div[@class=\"page-header-headings\"]")).getText();
+		addExternalTool(externalToolCustomName,externalToolVisibleName);
+		NavigateToLTI("'XAVIER 1.3'");
+		String provisionedCourseName=driver.findElement(By.xpath("//li[@class='nav-item active']")).getAttribute("title");
+		
+       if(provisionedCourseName.contains(moodleCourseName)){
+			reportStep("The moodle course is successfully autoprovisioned in yuja", "PASS", false);
+		} else {
+			reportStep("The moodle course is not successfully autoprovisioned in yuja", "FAIL", true);
+		}	
+	}
+	// Method directly used in test class.
+	public void checkManualprovisionOfNewCourse(String adminUserName, String adminPassword, String courseName, String courseShortName, String externalToolCustomName, String externalToolVisibleName, String userName, String password ) throws InterruptedException {
+		setAndResetAutomaticProvision(userName,password,"course",false);
+		setStartPageOptionAsMediaChannel();
+		createMoodleCourse(adminUserName, adminPassword,courseName,courseShortName);
+		addExternalTool(externalToolCustomName,externalToolVisibleName);
+		NavigateToLTI("'XAVIER 1.3'");
+		clickElement("Create new course button", By.xpath("//a[@title=\"Create a Course\"]"));
+		waitForElement(By.xpath("//input[@id=\"className\"]"),10);
+		sendKeys("Enter manual course full name", By.xpath("//input[@id=\"className\"]"),courseName+getRandomInteger(1000));
+		sendKeys("Enter manual course short name", By.xpath("//input[@id=\"classCode\"]"),courseShortName+getRandomInteger(1000));
+		clickElement("Create new course button", By.xpath("//button[@data-automation=\"btnCreateYourGroup\"]"));
+		waitForElement(By.xpath("//li[@class='nav-item active']"),10);
+		String provisionedCourseName=driver.findElement(By.xpath("//li[@class='nav-item active']")).getAttribute("title");
+		
+       if(provisionedCourseName.contains(courseName)){
+			reportStep("The moodle course is successfully manuallyprovisioned in yuja", "PASS", false);
+		} else {
+			reportStep("The moodle course is not successfully manuallyprovisioned in yuja", "FAIL", true);
+		}	
+	}
+	
+	// Method directly used in test class.
+	public void checkManualprovisionToExistingCourse(String adminUserName, String adminPassword, String courseName, String courseShortName, String externalToolCustomName, String externalToolVisibleName, String userName, String password ) throws InterruptedException {
+		setAndResetAutomaticProvision(userName,password,"course",false);
+		setStartPageOptionAsMediaChannel();
+		createMoodleCourse(adminUserName, adminPassword,courseName,courseShortName);
+		addExternalTool(externalToolCustomName,externalToolVisibleName);
+		NavigateToLTI("'XAVIER 1.3'");
+		clickElement("Link to an existing course button", By.xpath("//a[@title=\"Link to an Existing Course\"]"));
+		waitForElement(By.xpath("//select[@id=\"classSelectBox\"]"),10);
+		clickElement("dropdown", By.xpath("//select[@id=\"classSelectBox\"]"));
+		clickElement("choose the auto existing course", By.xpath("//select[@id=\"classSelectBox\"]//option[text()='aec -  - AutoExistingCourse - ']"));
+		clickElement("Click OK button after selecting the existing course", By.xpath("//button[@title=\"OK\"]"));
+		waitForElement(By.xpath("//li[@class='nav-item active']"),15);
+		String provisionedCourseName=driver.findElement(By.xpath("//li[@class='nav-item active']")).getAttribute("title");
+		
+       if(provisionedCourseName.contains("AutoExistingCourse")){
+			reportStep("The moodle course is successfully manuallyprovisioned  and connected to existing course in yuja", "PASS", false);
+		} else {
+			reportStep("The moodle course is not successfully manuallyprovisioned connected to existing course in yuja", "FAIL", true);
+		}	
+	}
+	
+	public void checkAutoprovisionOfUser(String adminUserName, String adminPassword, String userName, String password, String email, String newUsername, String newUserPassword, String courseName, String role, String courserole ) throws InterruptedException {
+		
+		HashMap<String, String> CourseRoleType = new HashMap<String, String>();
+		CourseRoleType.put("GroupMember", "//div[@data-automation=\"divGroupMembers\"]//div[@class=\"user-list-item\"]");
+		CourseRoleType.put("GroupOwner", "//div[@data-aitomation=\"divGroupOwners\"]//div[@class=\"user-list-item\"]");
+		
+		setAndResetAutomaticProvision(userName,password,"user",true);
+		String studentUserName=createMoodleUser(adminUserName, adminPassword,email,newUsername,newUserPassword);
+		selectCourseFromSiteHome(courseName);
+		
+		clickElement("participants", By.xpath("//a[@data-key=\"participants\"]"));
+		clickElement("enroll users", By.xpath("//input[@value=\"Enrol users\"]"));
+		sendKeys("Enter username", By.xpath("(//input[@class=\"form-control\" and contains(id,form_autocomplete_input-1677790244437)])[2]"),studentUserName);
+		clickElement("click from Dropdown", By.xpath("//ul[@class=\"form-autocomplete-suggestions\"]//li"));
+		
+		Select assignRoledropdown = new Select(driver.findElement(By.id("id_roletoassign")));
+		assignRoledropdown.selectByVisibleText(role);
+		clickElement("enroll selected users button", By.xpath("//button[text()=\"Enrol selected users and cohorts\"]"));
+		logout();
+		navigateToLoginPage();
+		driver.manage().window().maximize();
+		loginFast(studentUserName, newUserPassword);
+		selectCourseFromSiteHome(courseName);
+		NavigateToLTI("'XAVIER 1.3'");
+		waitForElement(By.xpath("//*[@id=\"bi_userInfoDropdown\"]"),15);
+		navbar.clickMyAccountButton();
+		navbar.clickMyAccountDropdownOption();
+		
+		String myaccountStudentName=driver.findElement(By.xpath("//input[@id='firstName']")).getAttribute("value");
+		navbar.clickCoursesAndGroups();
+		sendKeys("enter course name",By.xpath("//input[@class=\"group-management-inputbox form-control search-box\"]"),courseName);
+		clickElement("choose course after search",By.xpath("//ul[@id=\"courses-and-groups\"]//li"));
+		List<WebElement> memberList= getElementList(By.xpath(CourseRoleType.get(courserole)));
+		String obtainedmemberFirstname=null;
+		for(int i=0;i<memberList.size();i++) {
+			WebElement element=memberList.get(i);
+			String obtainedmemberName = element.getText();
+			String obtainedmembersplitFirstname[]=obtainedmemberName.split(" ",2);
+		    obtainedmemberFirstname=obtainedmembersplitFirstname[0];
+		    System.out.println(obtainedmemberFirstname);
+			if(obtainedmemberFirstname.equals(studentUserName) && obtainedmemberFirstname.equals(myaccountStudentName)){
+				reportStep("The moodle user is enrolled to  course in yuja", "PASS", false);
+				break;
+				
+			}}
+	
+		if(obtainedmemberFirstname.equals(myaccountStudentName) && obtainedmemberFirstname.equals(studentUserName)){
+			System.out.println(myaccountStudentName+" "+studentUserName + " "+obtainedmemberFirstname );
+			reportStep("The moodle user is successfully provisioned  and enrolled to  course in yuja", "PASS", false);
+		}
+	
+		else {
+			reportStep("The moodle user is  not successfully provisioned  and enrolled to  course in yuja", "FAIL", true);
+			}
+	}
+
+
+	
 	//Check media exists in the media library deletion only work for last uploaded media
 	
 	public void checkMediaExitsinMediaLibrary(List<String> mediaTitlelist) throws InterruptedException {
@@ -227,6 +434,17 @@ public class MoodlePage extends QuizPageHelpers {
 		clickElement("select content", By.cssSelector("button[name=\"selectcontent\"]"));
         Thread.sleep(3000);
 		switchToIframe("switch to mediachooser frame", By.id("contentitem-page-iframe"), 10);
+	}
+	
+	private void addExternalTool(String externalToolCustomName, String externalToolVisibleName) throws InterruptedException {
+		clickElement("Turn Editing on button", By.cssSelector("button[id^=\"single_button\"]"));
+		clickElement("add activity or resource button", By.cssSelector("span[class=\"section-modchooser-text\"]"));
+		Thread.sleep(3000);
+		clickElement("External tool", By.xpath("//a[@title='Add a new External tool']"));
+		sendKeys("Enter external tool name", By.xpath("//input[@name='name']"),externalToolCustomName);
+		Select toolDropdown = new Select(driver.findElement(By.xpath("//select[@name='typeid']")));
+		toolDropdown.selectByVisibleText(externalToolVisibleName);
+		clickElement("save and display button", By.xpath("//input[@name='submitbutton2']"));
 	}
 	
 	
